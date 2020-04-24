d55eb0d5b0fa57605f4751cdf8593592dce3cf5b
Fábio Oliveira <fabio.oliveira2@estudante.ufla.br>
2020-04-24 18:03:13 -0300

    :hammer: Remove Footer do design antigo.

--- src/pages/home/Main.jsx
+++ src/pages/home/Main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Navbar from '../../components/Navbar/Nav'
 import Routes from './Routes'
-import Footer from '../../components/Footer/Footer'
+//import Footer from '../../components/Footer/Footer'
 
 export default class main extends Component {
   
@@ -15,7 +15,6 @@ export default class main extends Component {
         <div className='main-container'>
           <Navbar {...this.props} />
           <Routes />
-          <Footer />
         </div>
       </BrowserRouter>
     )
