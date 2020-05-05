6ff6a08346f12d97d54a95d572a33775abc88f74
Fábio Oliveira <fabio.oliveira2@estudante.ufla.br>
2020-05-05 13:41:29 -0300

    :hammer: Refatora Display de Produtos da Dispensa.

--- src/components/templates/Dispensation/Dispensation.jsx
+++ src/components/templates/Dispensation/Dispensation.jsx
@@ -25,7 +25,7 @@ function Dispensation() {
             <p><img src={AddIcon} alt='add-icon' />Adicionar um Novo Produto</p>
             <div className="row">
               <Label color='pictonBlue' title='Nome do Produto' />
-              <Input color='pictonBlue' placeholder='Produto...'/>
+              <Input color='pictonBlue' placeholder='Produto...' />
               <Button type='submit' title='Cadastrar' color='mediumseaGreen' />
               <Button type='button' title='Limpar' color='orangeWeb' />
             </div>
@@ -33,13 +33,17 @@ function Dispensation() {
               <div className="dispensation-content-products-all">
                 <p><img src={StoreIcon} alt='store-icon' />Produtos da Dispensa</p>
                 <div className="dispensation-content-products-all-itens">
-                  
+                  <div className='dispensation-content-products-all-itens-list'>
+                    
+                  </div>
                 </div>
               </div>
               <div className="dispensation-content-products-all">
                 <p><img src={StoreCarFoundIcon} alt='store-car-found-icon' />Produtos em Falta</p>
                 <div className="dispensation-content-products-all-missing">
-                  
+                  <div className='dispensation-content-products-all-missing-list'>
+                   
+                  </div>
                 </div>
               </div>
             </div>
